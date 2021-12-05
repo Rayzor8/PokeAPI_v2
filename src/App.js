ca962cdf3892ad0d449f86f555d052960b9be65f
flashback220 <rayhidayat220@gmail.com>
2021-12-05 22:49:38 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const API_ENDPOINT = 'https://pokeapi.co/api/v2'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-gray-600 text-white">
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit neque in nostrum tempora cumque voluptatibus. Expedita ea rerum corrupti corporis.</h1>
     </div>
   );
 }
